3bdb759099a694ff9e55c3ca6103254d07ee0b2c
Jonathan Xu <jonxu100@gmail.com>
2024-02-11 06:49:42 -0500

    readme

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def generate_chart(risk_slider):
     # ind = bisect.bisect_left(ind_map.keys(), risk_slider)
     risk_slider = round(risk_slider, 3)
     df = pd.DataFrame(zip(names, weights.iloc[ind_map[risk_slider]].values), columns = ['name', 'weight'])
-    fig = px.pie(df.sort_values('weight', ascending = [False]).head(10), values='weight', names='name', hole=0.3, labels=None)
+    fig = px.pie(df.sort_values('weight', ascending = [False]), values='weight', names='name', hole=0.3, labels=None)
     fig.update_layout(
         font=dict(family="Times New Roman", size=20, color="RebeccaPurple")  # Update font here
     )
